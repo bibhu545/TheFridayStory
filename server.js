3ae2b94336b10789db52707219171f2036d82b6d
Bibhu Ranjan <bibhuranjan.500@gmail.com>
2020-10-15 23:16:14 +0530

    Initial Setup
    Added Angular app and api routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+// Get our API routes
+const homeRoutes = require('./api/routes/homeRoutes');
+// const articleRoutes = require('./api/routes/articleRoutes');
+
+const app = express();
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+// app.use('/api/articles', articleRoutes);
+app.use('/api', homeRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.listen(port);
