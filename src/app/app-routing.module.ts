298a1160b4d202d3a70e404d62dd1efa84877e23
Bibhu Ranjan <bibhuranjan.500@gmail.com>
2020-10-17 00:09:19 +0530

    Added Home page UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { MasterComponent } from './Components/master/master.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MasterComponent, children: [
+      { path: '', component: HomeComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { MasterComponent } from './Components/master/master.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { RightPanelComponent } from './Components/right-panel/right-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    MasterComponent,
+    FooterComponent,
+    RightPanelComponent
   ],
   imports: [
     BrowserModule,
